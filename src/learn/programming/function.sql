d9e2f4c6f62fb13f3f8b02f9e3d9779e4e192d9c
rushmi0 <gcalku@gmail.com>
2025-05-26 17:07:43 +0700

    new update

---
+++ src/learn/programming/function.sql
@@ -0,0 +1,37 @@
+﻿USE AdventureWorks2019
+GO
+
+-- ลบฟังก์ชันเดิมหากมี
+IF OBJECT_ID('dbo.fn_CompoundInterest', 'FN') IS NOT NULL
+    DROP FUNCTION dbo.fn_CompoundInterest;
+GO
+
+-- สร้างฟังก์ชัน
+CREATE FUNCTION dbo.fn_CompoundInterest
+(
+    @Principal MONEY,        -- เงินต้น
+    @Rate FLOAT,             -- อัตราดอกเบี้ย เช่น 0.05 (5%)
+    @Years INT               -- จำนวนปี
+)
+RETURNS MONEY
+AS
+BEGIN
+    DECLARE @Amount MONEY;
+
+    SET @Amount = @Principal * POWER((1 + @Rate), @Years);
+
+    RETURN @Amount;
+END;
+GO
+
+
+SELECT dbo.fn_CompoundInterest(10000, 0.05, 10) AS FutureAmount;
+
+
+SELECT 
+    p.FirstName,
+    p.LastName,
+    e.Rate AS StartingRate,
+    dbo.fn_CompoundInterest(e.Rate, 0.05, 10) AS RateIn10Years
+FROM HumanResources.EmployeePayHistory e
+	JOIN Person.Person p ON e.BusinessEntityID = p.BusinessEntityID;
---
+++ src/learn/programming/variables.sql
@@ -0,0 +1,53 @@
+USE AdventureWorks2019
+GO
+
+DECLARE @x INT;
+SET @x = 10;
+
+SELECT @x AS MyVariable;
+
+
+DECLARE @y INT = 32;
+SELECT @y AS MyData;
+
+
+
+DECLARE @Gender CHAR(1);
+SET @Gender = 'M';
+
+SELECT BusinessEntityID, JobTitle, Gender
+FROM HumanResources.Employee
+WHERE Gender = @Gender;
+
+
+
+DECLARE @MinRate MONEY = 20.00;
+DECLARE @MaxRate MONEY = 50.00;
+
+SELECT p.FirstName, p.LastName, e.Rate
+FROM HumanResources.EmployeePayHistory e
+	JOIN HumanResources.Employee emp ON e.BusinessEntityID = emp.BusinessEntityID
+	JOIN Person.Person p ON emp.BusinessEntityID = p.BusinessEntityID
+WHERE e.Rate BETWEEN @MinRate AND @MaxRate;
+
+
+
+SELECT 'Hello';
+PRINT 'MSSQL';
+
+
+DECLARE @a FLOAT;
+SET @a = dbo.fn_CompoundInterest(10000, 0.05, 10);
+PRINT @a; -- 16288.9
+
+PRINT CAST(ROUND(@a, 2) AS VARCHAR(20));
+
+
+
+SELECT @@SERVERNAME
+SELECT @@VERSION
+
+SELECT * FROM Production.Product;
+SELECT @@ROWCOUNT
+
+
