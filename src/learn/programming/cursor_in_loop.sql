10e28291f5f1574ab6dd41d2595f03b00baa33c2
rushmi0 <gcalku@gmail.com>
2025-05-27 16:45:15 +0700

    update sql

---
+++ src/learn/programming/cursor_in_loop.sql
@@ -0,0 +1,52 @@
+﻿USE adventureworks2019;
+GO
+
+DECLARE @EmployeeID INT;
+-- ประกาศ Cursor
+DECLARE employee_cursor CURSOR FOR
+    SELECT businessentityid
+    FROM humanresources.employee;
+
+-- เปิด Cursor
+OPEN employee_cursor;
+
+-- ดึงข้อมูลแถวแรก
+FETCH next FROM employee_cursor INTO @EmployeeID;
+
+-- วนลูปผ่านข้อมูล
+WHILE @@FETCH_STATUS = 0
+    BEGIN
+        PRINT 'Employee ID: ' + Cast(@EmployeeID AS VARCHAR);
+
+        -- ดึงข้อมูลแถวถัดไป
+        FETCH next FROM employee_cursor INTO @EmployeeID;
+    END;
+
+-- ปิดและล้าง Cursor
+CLOSE employee_cursor;
+
+DEALLOCATE employee_cursor;
+
+
+---
+
+
+DECLARE @EmployeeID INT;
+
+DECLARE employee_cursor CURSOR FAST_FORWARD FOR
+    SELECT BusinessEntityID
+    FROM HumanResources.Employee;
+
+OPEN employee_cursor;
+
+FETCH NEXT FROM employee_cursor INTO @EmployeeID;
+
+WHILE @@FETCH_STATUS = 0
+    BEGIN
+        PRINT 'Employee ID: ' + CAST(@EmployeeID AS VARCHAR);
+        FETCH NEXT FROM employee_cursor INTO @EmployeeID;
+    END;
+
+CLOSE employee_cursor;
+DEALLOCATE employee_cursor;
+
--- src/learn/programming/loop.sql
+++ src/learn/programming/loop.sql
@@ -1,10 +1,24 @@
 USE AdventureWorks2019;
 GO
 
-USE AdventureWorks2019;
-GO
-
 DECLARE @MaxVacationHours INT;
+DECLARE @Marker INT;
+DECLARE @NumEmployees INT;
 
 SET @MaxVacationHours = (SELECT MAX(VacationHours) FROM HumanResources.Employee);
-SELECT @MaxVacationHours AS [Max Vacation Hours];
+
+SET @Marker = 0;
+
+WHILE @Marker <= @MaxVacationHours
+	BEGIN
+		SET @NumEmployees = (
+			SELECT  COUNT(*) FROM HumanResources.Employee WHERE VacationHours = @Marker
+		)
+
+		 IF @Marker = 20 BREAK
+
+		PRINT CAST(@NumEmployees AS VARCHAR(3)) + ' employees still have '
+			+ CAST(@Marker AS VARCHAR(2)) + ' Vacation Hours'
+		SET @Marker = @Marker + 1
+	END
+
