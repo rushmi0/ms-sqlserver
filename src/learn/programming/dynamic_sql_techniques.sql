207e9353b6adeb122c82c5a6395fa612791bdd44
rushmi0 <gcalku@gmail.com>
2025-05-29 14:10:26 +0700

    update: Number Concatenation

--- src/learn/programming/dynamic_sql_techniques.sql
+++ src/learn/programming/dynamic_sql_techniques.sql
@@ -1,15 +1,19 @@
 USE [AdventureWorks2019]
 GO
 
+
+
 -- Dynamic SQL Techniques
 
 EXEC ('SELECT * FROM [Production].[Product]');
 
-EXEC sp_executesql N'SELECT * FROM [Production].[Product]';
+EXEC sp_executesql N'SELECT * FROM [Production].[Product]'
+
+SELECT N'นึกแล้ว มึงต้องอ่าน' AS ภาษาไทย;
+SELECT 'นึกแล้ว มึงต้องอ่าน' AS ภาษาไทย;
 
 
--- # SQL String concatenation
-
+-- # SQL String Concatenation
 DECLARE @TableName NVARCHAR(128);
 DECLARE @SQLString NVARCHAR(MAX)
 
@@ -19,5 +23,15 @@ SET @SQLString = 'SELECT * FROM ' + @TableName;
 EXEC sp_executesql @SQLString;
 
 
+-- # Number Concatenation
+DECLARE @Number INT;
+DECLARE @NumberString NVARCHAR(4);
+DECLARE @SQLString NVARCHAR(MAX);
 
 
+SET @Number = 15;
+SET @NumberString = CAST(@Number AS NVARCHAR(4));
+
+SET @SQLString = 'SELECT TOP ' + @NumberString + ' * FROM [Production].[Product] ORDER BY SellStartDate DESC';
+
+EXEC sp_executesql @SQLString;
