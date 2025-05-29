6c914583743b5c9b4bcbc3f1b509c077e04586a2
rushmi0 <gcalku@gmail.com>
2025-05-29 17:23:31 +0700

    new: transaction

---
+++ src/learn/programming/transaction.sql
@@ -0,0 +1,33 @@
+USE [AdventureWorks2019]
+GO
+
+-- ฺฺBEGIN TRAN
+BEGIN TRANSACTION
+
+SELECT
+    BusinessEntityID,
+    JobTitle
+FROM
+    HumanResources.Employee
+WHERE BusinessEntityID = 7;
+
+
+UPDATE HumanResources.Employee
+SET JobTitle = 'Data Scientist 2'
+WHERE BusinessEntityID = 7;
+
+
+SELECT
+    BusinessEntityID,
+    JobTitle
+FROM
+    HumanResources.Employee
+WHERE BusinessEntityID = 7;
+
+-- ถ้าใช้ COMMIT ไปอล้วจะไม่สามารถ ROLLBACK
+-- บันทึกการเปลี่ยนแปลง
+COMMIT -- COMMIT TRAN, COMMIT TRANSACTION
+
+-- ยกเลิกการเปลี่ยนแปลง
+ROLLBACK TRANSACTION
+
