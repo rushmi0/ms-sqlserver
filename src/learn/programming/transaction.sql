9e8ca0aad15b73aa19017e5d51fe11f2b081c36e
rushmi0 <gcalku@gmail.com>
2025-05-30 11:06:18 +0700

    add: Conditionally committing or rolling back (Beginning Script)

--- src/learn/programming/transaction.sql
+++ src/learn/programming/transaction.sql
@@ -32,12 +32,70 @@ COMMIT -- COMMIT TRAN, COMMIT TRANSACTION
 ROLLBACK TRANSACTION
 
 
--- #Nmaine Transaction
+-- # Naming Transaction
 BEGIN TRANSACTION UpdateJobTitle
 
 UPDATE HumanResources.Employee
-SET JobTitle = 'DevOps Engineer'
+SET JobTitle = 'DevOps Engineer' -- Application Specialist, DevOps Engineer
 WHERE BusinessEntityID = 268;
 
+COMMIT TRANSACTION UpdateJobTitle
 
+
+SELECT JobTitle FROM HumanResources.Employee WHERE BusinessEntityID = 268;
+
+
+
+
+
+-- # IF Statement to commit or rollback
+IF OBJECT_ID('dbo.books', N'U') IS NOT NULL
+    DROP TABLE dbo.books;
+GO
+
+CREATE TABLE dbo.books
+(
+    id INT IDENTITY (1, 1),
+    book_name VARCHAR(100) NOT NULL ,
+    sold_copies INT NOT NULL
+);
+
+
+-- Insert data
+INSERT INTO dbo.books (book_name, sold_copies)
+VALUES ('book_1', 10),
+       ('book_2', 30),
+       ('book_3', 15),
+       ('book_4', 20);
+
+
+SELECT * FROM dbo.books;
+DELETE FROM books WHERE book_name = 'book_5'
+
+
+-- # Conditionally committing or rolling back (Beginning Script)
+DECLARE @BookName INT;
+
+BEGIN TRANSACTION AddBook;
+
+DECLARE @NameNameData VARCHAR(50) = 'book_5';
+DECLARE @SoldCopiesData INT = 25;
+
+
+
+INSERT INTO dbo.books (book_name, sold_copies)
+VALUES (@NameNameData, @SoldCopiesData);
+
+SELECT @BookName = COUNT(*) FROM dbo.books WHERE book_name = 'book_5';
+
+IF @BookName > 1
+    BEGIN
+       ROLLBACK TRANSACTION AddBook;
+       PRINT @NameNameData + ' already exists.';
+    END
+ELSE
+    BEGIN
+        COMMIT TRANSACTION AddBook
+        PRINT @NameNameData + ' was added to the books table.'
+    END
 
